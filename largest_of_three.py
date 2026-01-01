8b976ada6a1d7bfe5cca39a9879c27e0dbe98aa5
Sumedha494 <sumedhasadaphal02@gmail.com>
2026-01-01 09:42:28 +0530

    Create largest_of_three.py

---
+++ largest_of_three.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 10
+
+
+# In[2]:
+
+
+b = 25
+
+
+# In[3]:
+
+
+c = 15
+
+
+# In[4]:
+
+
+largest = max(a, b, c)
+
+
+# In[5]:
+
+
+print(f"The largest number is: {largest}")
+
+
+# In[ ]:
+
+
+
+
