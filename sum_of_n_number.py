fe296d216e679ecd8bf4c03363392e5153840c66
Sumedha494 <sumedhasadaphal02@gmail.com>
2026-01-02 20:42:33 +0530

    Create sum_of_n_number.py

---
+++ sum_of_n_number.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n = 10
+
+
+# In[2]:
+
+
+sum = 0
+
+
+# In[3]:
+
+
+for i in range(1, n+1):
+    sum = sum + i
+
+
+# In[4]:
+
+
+print(f"Sum of first {n} numbers is: {sum}")
+
