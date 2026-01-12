16f58f0d305e64a13b739514776102576f2673bc
Sumedha494 <sumedhasadaphal02@gmail.com>
2026-01-12 13:33:49 +0530

    Create armstrong_number.py

---
+++ armstrong_number.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+num = int(input("Enter a number:"))
+temp = num 
+sum = 0 
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+
+if sum == num:
+    print("Armstrong Number")
+else:
+    print("Not Armstrong")
+
+
