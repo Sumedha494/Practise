8d7ef87f7b36276c90040a47e24d7a66aba520ea
Sumedha494 <sumedhasadaphal02@gmail.com>
2026-01-18 09:53:42 +0530

    Create check_anagram.py

---
+++ check_anagram.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+s1 = input("Enter first string: ")
+s2 = input("Enter second string: ")
+
+if sorted(s1) == sorted(s2):
+    print("Anagram")
+else:
+    print("Not Anagram")
+
