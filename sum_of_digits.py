6ae1568461bd7415155e83197db42abbfc185cae
Sumedha494 <sumedhasadaphal02@gmail.com>
2026-01-26 21:21:53 +0530

    Create sum_of_digits.py

---
+++ sum_of_digits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+num = int(input("Enter a number: "))
+total = 0
+
+while num>0:
+    total += num % 10
+    num //= 10
+
+print("Sum of digits: ", total)
+
